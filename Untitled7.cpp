81d6d5264c10faa2d540c2eb92cbf77b6e5debd7
Aluno <Aluno@CT-LAB1-16.LABUVA.EDU>
2016-11-24 21:05:40 -0200

    novo

--- Untitled7.cpp
+++ Untitled7.cpp
@@ -4,12 +4,25 @@ main()
 {
 	int matriz[2][4],a=0,b=0,c,d;
 	inicio:
-	for(c=1;c<9;c++){
-		printf("\nDigite um numero: ");
-		scanf("%d",&d);
-		if((d<0) || (d>20)){
+		for(c=1;c<9;c++){
+			printf("\nDigite um numero: ");
+			scanf("%d",&d);
+			if((d<0) || (d>20)){
 			printf("Numero invalido\n");
 			goto inicio;
+			}
+	}
+	for(b=0;b<4;b++){
+	if(d<=11){
+		matriz[0][b] = d;
+	}
+	else if((d>11) || (d<=20)){
+	matriz[1][b]=d;
+	}
+	}
+	for(a=0;a<2;a++){
+		for(b=0;b<4;b++){
+			printf("%d",matriz[a][b]);
 		}
 	}
 }
