a234bb156db7dae31e8641f9019fe479b68947e0
Aluno <Aluno@CT-LAB1-16.LABUVA.EDU>
2016-11-24 21:25:01 -0200

    salvei meu novo codigo

--- Untitled7.cpp
+++ Untitled7.cpp
@@ -2,27 +2,17 @@
 #include<stdio.h>
 main()
 {
-	int matriz[2][4],a=0,b=0,c,d;
-	inicio:
-		for(c=1;c<9;c++){
-			printf("\nDigite um numero: ");
-			scanf("%d",&d);
-			if((d<0) || (d>20)){
-			printf("Numero invalido\n");
-			goto inicio;
+	int matriz[2][4],a=0,b=0,cont=0,num;
+	while(c<=8){
+		printf("Digite um valor: ";
+		scanf("%d",&num);
+		if((num<0) || (num>20)){
+			printf("\nNumero INVALIDO");
+		}
+		else if(num<11){
+			while(b<4){
+				
 			}
-	}
-	for(b=0;b<4;b++){
-	if(d<=11){
-		matriz[0][b] = d;
-	}
-	else if((d>11) || (d<=20)){
-	matriz[1][b]=d;
-	}
-	}
-	for(a=0;a<2;a++){
-		for(b=0;b<4;b++){
-			printf("%d",matriz[a][b]);
 		}
 	}
 }
